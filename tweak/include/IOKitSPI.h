cdba74b813ab55c5586792bac2de67dbe94e19aa
hogan-hong <hogan_hong@icloud.com>
2026-04-09 00:33:55 +0800

    Add tweak/include/IOKitSPI.h

---
+++ tweak/include/IOKitSPI.h
@@ -0,0 +1,11 @@
+/*
+ * IOKitSPI.h - IOKit SPI declarations for IOSurface
+ */
+
+#ifndef IOKitSPI_h
+#define IOKitSPI_h
+
+#include <IOKit/IOReturn.h>
+#include <IOKit/IOTypes.h>
+
+#endif /* IOKitSPI_h */
