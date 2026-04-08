944ecf85566bfbe1d3d605d675bbbf900d700644
hogan-hong <hogan_hong@icloud.com>
2026-04-09 00:33:34 +0800

    Add tweak/src/StreamTweak.h

---
+++ tweak/src/StreamTweak.h
@@ -0,0 +1,15 @@
+/*
+ * iOSScreenStream - Main tweak entry
+ */
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StreamTweak : NSObject
+
++ (void)load;
+
+@end
+
+NS_ASSUME_NONNULL_END
